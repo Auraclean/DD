b7ed2afc326af20ae7cd467f767e1596bb2138d8
Le Nay Yann <yann.le.nay@gmail.com>
2021-05-14 12:37:51 +0200

    dao propre

--- Maven project/DD_Project/src/main/java/dao/DAOItem.java
+++ Maven project/DD_Project/src/main/java/dao/DAOItem.java
@@ -1,74 +1,41 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import metier.Item;
 import util.Context;
-
-
-
-
-
 
 public class DAOItem implements IDAO<Item,Integer>{
 
 	public Item findById(int id) {
-		
-		
 		EntityManager em = Context.get_instance().getEmf().createEntityManager();
-		
 		Item item=em.find(Item.class, id);
-		
 		em.close();
-		
 		return item;
-		
 	}
 
 public List<Item> findAll() {
-		
-	EntityManager em = Context.get_instance().getEmf().createEntityManager();
-		
-		List<Item> myItems = em.createQuery("select i from Item i", Item.class).getResultList();
-		
-		em.close();
-		
+	EntityManager em = Context.get_instance().getEmf().createEntityManager();	
+		List<Item> myItems = em.createQuery("select i from Item i", Item.class).getResultList();		
+		em.close();		
 		return myItems;
 	}
 
 	public Item save(Item item) {
-		
-		EntityManager em = Context.get_instance().getEmf().createEntityManager();
-		
+		EntityManager em = Context.get_instance().getEmf().createEntityManager();	
 		em.getTransaction().begin();
 		item=em.merge(item);
-		em.getTransaction().commit();
-		
-		
+		em.getTransaction().commit();	
 		em.close();
 		return item;
 	}
 
-	public void delete(Item item) {
-		
-		EntityManager em = Context.get_instance().getEmf().createEntityManager();
-		
+	public void delete(Item item) {	
+		EntityManager em = Context.get_instance().getEmf().createEntityManager();	
 		em.getTransaction().begin();
 		item=em.merge(item);
 		em.remove(item);
-		em.getTransaction().commit();
-		
+		em.getTransaction().commit();	
 		em.close();
-
 	}
-	
 }
